9edcefbc3eb9713ebb7ea93667f2ac321426330a
M.Shami <mohammad-dyaa-addin.shami@iu-study.org>
2023-07-13 20:00:54 +0200

    meine Solution

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+prime = 2
+counter = 0
+
+
+def isprime():
+    for indexFor in range(2, prime):
+        if prime % indexFor == 0:
             return False
     return True
 
 
-def find_thousandth_prime():
-    count = 0
-    num = 2
-    while count < 1000:
-        if is_prime(num):
-            count += 1
-        num += 1
-    return num - 1
+while counter < 1000:
+    if isprime():
+        counter += 1
+    prime += 1
 
-
-thousandth_prime = find_thousandth_prime()
-print(thousandth_prime)
+print(prime - 1)
